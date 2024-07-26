e74ce71de3d7ec7abb0754cc6473ebaf17e9146c
Md. Shofiul Islam <ytshofiul@gmail.com>
2024-07-26 13:02:04 +0600

    Validation pipe added in the project

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
 
-@Controller('messages')
-export class MessagesController {}
+@Controller('/messages')
+export class MessagesController {
+  @Get()
+  listMessages() {
+    return [];
+  }
+  @Post()
+  createMessage(message: string) {
+    return '';
+  }
+  @Get('/:id')
+  getMessage() {
+    return 'Message';
+  }
+}
